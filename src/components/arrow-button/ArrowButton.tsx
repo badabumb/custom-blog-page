72fb82ded728d7e648fcf1ee5578e75e1bf5e8c8
badabumb <juliavediukova@mail.ru>
2025-04-14 14:04:06 +0300

    change select, arrowbutton and articleParamsForm

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -7,19 +7,24 @@ import styles from './ArrowButton.module.scss';
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+interface ArrowButtonProps {
+	open: boolean;
+	setOpen: (value: boolean | ((prev: boolean) => boolean)) => void;
+}
+export const ArrowButton = ({ open, setOpen }: ArrowButtonProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}
+			className={clsx(styles.container, open && styles.container_open)}
+			onClick={() => setOpen((prev) => !prev)}
 		>
 			<img
 				src={arrow}
 				alt='иконка стрелочки'
-				className={styles.arrow}
+				className={clsx(styles.arrow, open && styles.arrow_open)}
 			/>
 		</div>
 	);
